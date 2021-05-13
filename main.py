ae6d8ca3dcf4054c51ff2af9e0e85bb32110e17e
Daniel Ting <danielzting@gmail.com>
2021-05-12 23:24:16 -0500

    Add ability to delete messages with trash reaction

--- main.py
+++ main.py
@@ -6,13 +6,31 @@ from unalix import clear_url
 
 class MyClient(discord.Client):
     async def on_message(self, message):
-        urls = re.findall("(?P<url>https?://[^\s]+)", message.content)
+        # Sanity check for safety to prevent infinite recursion
+        if message.author == client.user:
+            return
+
+        # Extract links and clean
+        urls = re.findall('(?P<url>https?://[^\s]+)', message.content)
         cleaned = []
         for url in urls:
             if clear_url(url) != url:
                 cleaned.append(clear_url(url))
+
+        # Send message and add reactions
         if cleaned:
-            await message.reply("It appears that you have sent one or more links with tracking parameters. Below are the same links with those fields removed:\n" + "\n".join(cleaned))
+            text = 'It appears that you have sent one or more links with tracking parameters. Below are the same links with those fields removed:\n' + '\n'.join(cleaned)
+            reply = await message.reply(text, mention_author=False)
+            await reply.edit(suppress=True)
+            await reply.add_reaction('🗑')
+
+    async def on_reaction_add(self, reaction, user):
+        # Get author of original message
+        channel = client.get_channel(reaction.message.reference.channel_id)
+        original = await channel.fetch_message(reaction.message.reference.message_id)
+        # Delete message if original author clicked on trash reaction
+        if reaction.message.author == client.user and user == original.author and reaction.emoji == '🗑':
+            await reaction.message.delete()
 
 load_dotenv()
 client = MyClient()
