7f3f610d2e4f215a2493a654d6aeab8a804b0880
Daniel Ting <danielzting@gmail.com>
2021-05-15 00:13:18 -0500

    Optional react to delete and suppress embed perms

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# clearurls-discord-bot
+# ClearURLs Discord Bot
 A Discord bot that uses ClearURLs to automatically strip out tracking parameters from messages with links.
--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from unalix import clear_url
 
 class MyClient(discord.Client):
     async def on_message(self, message):
-        # Sanity check for safety to prevent infinite recursion
         if message.author == client.user:
-            return
+            permissions = message.channel.guild.me.permissions_in(message.channel)
+            # Add :wastebasket: emoji for easy deletion if necessary
+            if permissions.add_reactions and permissions.read_message_history:
+                await message.add_reaction('🗑')
+            # Suppress embeds for bot messages to avoid visual clutter
+            if permissions.manage_messages:
+                await message.edit(suppress=True)
 
         # Extract links and clean
         urls = re.findall('(?P<url>https?://[^\s]+)', message.content)
@@ -20,16 +25,17 @@ class MyClient(discord.Client):
         # Send message and add reactions
         if cleaned:
             text = 'It appears that you have sent one or more links with tracking parameters. Below are the same links with those fields removed:\n' + '\n'.join(cleaned)
-            reply = await message.reply(text, mention_author=False)
-            await reply.edit(suppress=True)
-            await reply.add_reaction('🗑')
+            await message.reply(text, mention_author=False)
 
     async def on_reaction_add(self, reaction, user):
-        # Get author of original message
-        channel = client.get_channel(reaction.message.reference.channel_id)
-        original = await channel.fetch_message(reaction.message.reference.message_id)
+        message = reaction.message
         # Delete message if original author clicked on trash reaction
-        if reaction.message.author == client.user and user == original.author and reaction.emoji == '🗑':
+        permissions = message.channel.guild.me.permissions_in(message.channel)
+        if not permissions.manage_messages or message.reference is None:
+            return
+        channel = client.get_channel(message.reference.channel_id)
+        original = await channel.fetch_message(message.reference.message_id)
+        if message.author == client.user and user == original.author and reaction.emoji == '🗑':
             await reaction.message.delete()
 
 load_dotenv()
